43e472245ed1dcc0247edf5031320e355c35d6fe
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-06-08 13:56:11 -0700

    MVP

--- queue/queue.py
+++ queue/queue.py
@@ -12,17 +12,51 @@ return elements in First In First Out order.
    
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
+
+
+### Queues
+ * Has the methods: `enqueue`, `dequeue`, and `len`.
+   * `enqueue` adds an element to the back of the queue.
+   * `dequeue` removes and returns the element at the front of the queue.
+   * `len` returns the number of elements in the queue.
 """
+from singly_linked_list import LinkedList
+
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if len(self.storage) == 0:
+#             return None
+#         else:
+#             return self.storage.pop(0)
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        pass
+        if self.__len__() == 0:
+            return None
+        else:
+            deq = self.storage.remove_head()
+            self.size -= 1
+            return deq
--- stack/stack.py
+++ stack/stack.py
@@ -9,17 +9,51 @@ return elements in Last In First Out order.
    Make sure the Stack tests pass.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
+
+
+### Stacks
+* Should have the methods: `push`, `pop`, and `len`.
+   * `push` adds an item to the top of the stack.
+   * `pop` removes and returns the element at the top of the stack
+   * `len` returns the number of elements in the stack.
 """
+from singly_linked_list import LinkedList
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) == 0:
+#             return None
+#         else:
+#             return self.storage.pop(-1)
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size += 1
 
     def pop(self):
-        pass
+        if self.__len__() == 0:
+            return None
+        else:
+            pop = self.storage.remove_tail()
+            self.size -= 1
+            return pop
